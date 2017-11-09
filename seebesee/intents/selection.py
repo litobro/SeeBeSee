27350c5adda4dc8ae233f0fa7eebe71b2772037f
Thomas Dang <thomas.kyle.dang@gmail.com>
2017-11-08 21:43:27 -0700

    Setup to stream Radio 1 Edmonton at start for testing

--- seebesee/intents/selection.py
+++ seebesee/intents/selection.py
@@ -1,14 +1,17 @@
 from flask_ask import statement, audio, question
 from seebesee import ask
+from seebesee.utils import stream_extractor
 
 @ask.launch
 def launch():
-    text = 'Welcome to See Be See Radio (Unofficial). \
-            Try asking me to play Radio One or Radio Two.'
+    text = 'Welcome to See Be See Radio (Unofficial).'
     prompt = 'For example say, play Radio One Edmonton'
+    '''
     return question(text).reprompt(prompt) \
         .simple_card(title='Welcome to CBC Radio (Unofficial)',
                      content='Try asking me to play a station')
+    '''
+    return audio(text).play('https://cbcliveradio-lh.akamaihd.net/i/CBCR1_EDM@372985/master.m3u8')
 
 @ask.intent("SeeBeSeePlayIntent")
 def play_station(station, city):
---
+++ seebesee/utils/stream_extractor.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+
+def get_stream(station, city):
+    pass
+
+def get_cities():
+    return []
